79972cf6ba84a777507204326663f3ae1e85637b
yuanjun17 <yuanjun918918@gmail.com>
2023-12-17 22:33:07 +0800

    commit

---
+++ EscapeFromShoeIndustry/Assets/script/ReLoad.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 监听用户输入，按下R键时重新加载场景
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadScene();
+        }
+    }
+    void ReloadScene()
+    {
+        // 获取当前场景的索引
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // 重新加载当前场景
+        SceneManager.LoadScene(currentSceneIndex);
+        Time.timeScale = 1f;
+    }
+}
